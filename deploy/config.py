8eb4c5e8072cfb066eb793a93130ec7a52b27956
stanhjr <stanhjrpower@gmail.com>
2022-05-27 13:17:56 +0300

    add deploy

---
+++ deploy/config.py
@@ -0,0 +1,12 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TOKEN = os.getenv('TOKEN')
+
+host = "localhost"
+DATABASE_URL = f'postgresql://stan:stan@localhost:5432/bot_test'
+
+ADMIN_IDS = [589380091, 436985071, 336609833]
