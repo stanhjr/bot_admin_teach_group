0b31b07d6a80bbcabff271617460dc2504751e69
stanhjr <stanhjrpower@gmail.com>
2022-05-27 13:23:04 +0300

    fix datebase_name

--- deploy/config.py
+++ deploy/config.py
@@ -7,6 +7,6 @@ load_dotenv()
 TOKEN = os.getenv('TOKEN')
 
 host = "localhost"
-DATABASE_URL = f'postgresql://stan:stan@localhost:5432/bot_test'
+DATABASE_URL = f'postgresql://stan:stan@localhost:5432/bot_teach'
 
 ADMIN_IDS = [589380091, 436985071, 336609833]
